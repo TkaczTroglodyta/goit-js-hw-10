9b615f127e364fd398a55646195f4eabaaa22854
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-03 22:29:19 +0200

    fix: delete sass folders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { fetchCatByBreed } from './js/cat-api';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfoPict = document.querySelector('.cat-info-pict');
@@ -9,10 +9,16 @@ const loaderEyes = document.querySelector('.loader');
 breedSelect.addEventListener('choice', onChoice);
 
 function onChoice(evt) {
-  loaderEyes.classList.remove('hidden');
+  loaderEyes.classList.remove('.hidden');
   catInfoPict.innerHTML = '';
   catInfoDescr.innerHTML = '';
   const breedId = evt.target.value;
   console.log('breedId: ', breedId); // Just for veryfing
-  fetchCatByBreed(breedId).then(breed => showBreedDesc);
+  fetchCatByBreed(breedId)
+    .then(breed => showBreedDesc(breed))
+    .catch(err => {
+      console.log(err);
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+    .finally(() => loaderEyes.classList.add('.hidden'));
 }
