dcf6a7915464edd623954797bf021965e954a817
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-03 20:48:34 +0200

    docs: create cat-api.js

---
+++ src/js/cat-api.js
@@ -0,0 +1,4 @@
+const url1 = 'https://api.thecatapi.com/v1/breeds';
+const url2 = 'https://api.thecatapi.com/v1/images';
+const API_KEY =
+  'api_key=live_3tP1CMH6ndx78LnNFYHfnKDgFZA1oF27NZ3FACVsBdgCi2qq0jBzYCR8Ikb6V26I';
