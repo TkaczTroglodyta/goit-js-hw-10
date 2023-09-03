456612791dd8524021a13fb5b29e30a734f9c08e
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-03 21:56:31 +0200

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import { fetchCatByBreed } from './js/cat-api';
+import Notiflix from 'notiflix';
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfoPict = document.querySelector('.cat-info-pict');
+const catInfoDescr = document.querySelector('.cat-info-desc');
+const loaderEyes = document.querySelector('.loader');
+
+breedSelect.addEventListener('choice', onChoice);
