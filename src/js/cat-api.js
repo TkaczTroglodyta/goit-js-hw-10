f47971e33256d46a8f55b92b2162fa5b131edae2
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-04 19:42:38 +0200

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,35 +1,51 @@
 const url1 = 'https://api.thecatapi.com/v1/breeds';
 const url2 = 'https://api.thecatapi.com/v1/images';
-const API_KEY =
+const CATAPI_KEY =
   'live_3tP1CMH6ndx78LnNFYHfnKDgFZA1oF27NZ3FACVsBdgCi2qq0jBzYCR8Ikb6V26I';
 
-// Funkcja fetchBreeds() wysyła żądanie do interfejsu API TheCatAPI w celu uzyskania listy ras kotów.
-// Używa funkcji fetch() do wysłania żądania HTTP do adresu URL ${url1}?api_key=${KEY},
-// gdzie ${url1} to adres bazowy interfejsu API do pobierania listy ras, a ${KEY} to klucz dostępu do interfejsu API.
-// Jeśli odpowiedź serwera nie wynosi 200 (OK), funkcja zgłasza błąd ze statusem odpowiedzi.
-// Jeśli odpowiedź się powiedzie, funkcja zwraca wynik w formacie JSON.
+// const fetchBreeds = () => {
+//   return fetch(`${url1}?api_key=${KEY}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// };
 
 const fetchBreeds = () => {
-  return fetch(`${url1}?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
+  return new Promise((resolve, reject) => {
+    fetch(`${url1}?api_key=${CATAPI_KEYKEY}`).then(response => {
+      if (!response.ok) {
+        resolve(response.status);
+      } else {
+        response.json().then(data => {
+          resolve(data);
+        });
+      }
+    });
   });
 };
 
-// Funkcja fetchCatByBreed() przyjmuje parametr breedId, który wskazuje identyfikator określonej rasy kota.
-// Wysyła żądanie do interfejsu API TheCatAPI o obraz kota wybranej rasy do czego używa funkcji fetch().
-// Żądanie HTTP przesyłane jest do adresu do adresu URL ${url2}/${breedId}?api_key=${API_KEY}.
-// Jeśli odpowiedź serwera nie wynosi 200 (OK), funkcja zgłasza błąd ze statusem odpowiedzi.
-// Jeśli odpowiedź się powiedzie, funkcja zwraca wynik w formacie JSON.
+// const fetchCatByBreed = breedId => {
+//   return fetch(`${url2}/${breedId}?api_key=${API_KEY}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// };
 
 const fetchCatByBreed = breedId => {
-  return fetch(`${url2}/${breedId}?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
+  return new Promise((resolve, reject) => {
+    fetch(`${url2}/${breedId}?api_key=${CATAPI_KEY}`).then(response => {
+      if (!response.ok) {
+        resolve(response.status);
+      } else {
+        response.json().then(data => {
+          resolve(data);
+        });
+      }
+    });
   });
 };
 
